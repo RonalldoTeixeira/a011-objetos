98290dc2233e170a2124c7466a2eaf25e49e038f
RonalldoTeixeira <ronalldo.tb@gmail.com>
2022-08-02 21:21:57 -0300

    respostas exer-11

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+
+//Crie um array vazio chamado sacolao
+const sacolao = []
+// Crie três objetos que vão representar frutas do sacolão
+const feira = {
+    fruta : 'uva',
+    preco: 5 ,
+    disponibilidade : false
+}
+console.log(feira)
+//Adicione os objetos ao array sacolao utilizando o método push()
+sacolao.push(feira)
+console.log(sacolao);
+
+//Criei os tres objetos, string, number e boolean depois adicionei a minha Array vazia o objeto "feira", com todas as propriedade dele.
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -11,5 +11,26 @@ const filme = {
 		{canal: "Globo", horario: "14h"}
 		]
 };
+/*a) Qual o código para imprimir o nome do **primeiro** ator/atriz?
 
+b) Qual o código para imprimir o nome do **último** ator/atriz?
+
+c) Qual o código para exibir o array com **todas** as transmissões de hoje?
+
+d) Qual o código para exibir o **horário** de transmissão do filme no `Canal Brasil`?*/
 // respostas aqui ⬇️
+
+//para acessar um caractere de string ou passar seu índice (variável [ e o numero correspondente e o numero do elemento ])
+//A)
+console.log(filme.elenco[0]);
+//B)
+console.log(filme.elenco[3,3]) 
+//const nomeDosCanais = filme.transmissoesHoje[0,2].canal
+//console.log(nomeDosCanais)
+//C)
+const {transmissoesHoje} = filme 
+console.log(transmissoesHoje);
+
+//D
+const horario = filme.transmissoesHoje[0,1].horario
+console.log(horario)
